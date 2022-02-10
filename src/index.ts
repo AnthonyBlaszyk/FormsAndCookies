5ed2b0a21480b0aeb65fdedc5c9c8366c0d37642
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-10 15:20:20 +0100

    Add light or dark mode choice with cookies

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import express from "express";
+import nunjucks from "nunjucks";
+import cookie from "cookie";
+
+const app = express();
+const formParser = express.urlencoded({ extended: true });
+let darkMode = false;
+let noCookie = true;
+
+app.use(express.static("public"));
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app.set("view engine", "njk");
+
+app.get("/", (request, response) => {
+  const cookies = cookie.parse(request.get("cookie") || "");
+
+  // check if we have cookie
+  if (cookies.myCookie === undefined) {
+    noCookie = true;
+  } else {
+    noCookie = false;
+  }
+
+  // check if cookie is set to darkMode
+  if (cookies.myCookie === "darkMode") {
+    darkMode = true;
+  } else {
+    darkMode = false;
+  }
+
+  response.render("home", { darkMode, noCookie });
+});
+
+app.get("/options", (request, response) => {
+  response.render("options", { darkMode, noCookie });
+});
+
+app.post("/handle-form", formParser, (request, response) => {
+  // request.body contains an object with our named fields
+  const cookieValue = Object.keys(request.body)[0];
+
+  if (cookieValue === "deleteCookie") {
+    response.set(
+      "Set-Cookie",
+      cookie.serialize("myCookie", "", {
+        maxAge: 0,
+      }),
+    );
+  } else {
+    response.set(
+      "Set-Cookie",
+      cookie.serialize("myCookie", cookieValue, {
+        maxAge: 3600, // This is the time (in seconds) that this cookie will be stored
+      }),
+    );
+  }
+
+  response.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
